3b45382946f0596a4f50d6619ff2b1213456007f
Demis Bellot <demis.bellot@gmail.com>
2017-08-11 02:12:19 -0400

    specify url

--- src/Todos/Startup.cs
+++ src/Todos/Startup.cs
@@ -23,6 +23,7 @@ namespace Todos
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
+                .UseUrls("http://localhost:5000/")
                 .Build();
     }
 
