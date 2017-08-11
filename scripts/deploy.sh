da6333a5d093c193e284740b76135c25bc00e89a
Demis Bellot <demis.bellot@gmail.com>
2017-08-11 00:30:34 -0400

    try installing python 2.7 in travis config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,10 +8,6 @@ export ECS_TASK=$IMAGE_NAME-task
 # install dependencies
 sudo apt-get install jq -y #install jq for json parsing
 sudo apt-get install gettext -y 
-
-sudo apt-get install libffi-dev libssl-dev # https://stackoverflow.com/a/29099439/85785
-pip install pyOpenSSL ndg-httpsclient pyasn1
-
 pip install --user awscli # install aws cli w/o sudo
 export PATH=$PATH:$HOME/.local/bin # put aws in the path
 
